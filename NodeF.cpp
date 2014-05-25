0983100fb638b814ddcf93966fe70483cc23fe8a
TechnoX <fredrik@eaproduktion.se>
2014-05-25 19:25:45 +0200

    Fixed Segfault bugs in shorten(), pointed the children to the zero instead of other child

--- NodeF.cpp
+++ NodeF.cpp
@@ -6,12 +6,23 @@ void Node::shorten(){
   shorten(this);
 }
 
+// Returns number of shortened steps
 int Node::shorten(Node* node){
+  // Shorten the children first (Note: Since it's a binary tree each node either have both or none children, so just check for one)
+  if(node->getRight()){
+    node->getRight()->shorten();
+    node->getLeft()->shorten();
+  }
+  else{
+    // If this node doesn't have any children we can't shorten it
+    return 0;
+  }
+  
   // If a child is 0, delete it
   // If this operator is + or -, move the other child to this node. 
   // If this operator is *, remove the other child as well
   // If this operator is /, if the 0 was to the right, remove node, else error
-  
+
   // Node containing a child that has data=0
   Node* zeroChild = NULL;
   // The other child (with unknown value)
@@ -34,17 +45,15 @@ int Node::shorten(Node* node){
       // If this node has an parent (isn't the ancestor too everyone)
       if(node->getParent()){
 	// ... move the other child to this node. 
-	node->getParent()->changeChild(node,zeroChild);
+	node->getParent()->changeChild(node,otherChild); // Changed 19:14 25 May, from (node,zeroChild) to (node,otherChild) Seems to work!
       }else{
 	// Move the other child to this position (without changing the address of this node, i.e. node = otherNode wont work)
 	node->setData(otherChild->getData());
 	node->setRight(otherChild->getRight(),node);
 	node->setLeft(otherChild->getLeft(),node);
-
 	// Delete the other node
 	delete otherChild;
       }
-
       break;
     case '/':
       if(zeroChild==node->getRight()){
@@ -64,13 +73,13 @@ int Node::shorten(Node* node){
       // ... remove the other child as well
       delete otherChild;
       node->setData("0");
+      node->setRight(NULL, node);
+      node->setLeft(NULL, node);
       break;
     }
   }
-
   // Delete the 0 (must be last because we are using this pointer to get right children in changeChild).
   delete zeroChild;
-  
   return 0;
 }
 
