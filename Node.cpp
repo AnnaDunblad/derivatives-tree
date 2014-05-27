fd2ea609b0e766550da86f65d48dec9d16c6089a
super_adun <anna.dunblad@gmail.com>
2014-05-27 14:32:16 +0200

    Indentation in Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -34,70 +34,62 @@ Node* Node::getLeft(){
   return _leftChildren;
 }
 
-
-
 void Node::printTree() {
   int maxLevel = this->maxLevel(this);
   printTreeInternal(std::vector<Node*>(1,this), 1, maxLevel);
 }
 
 void Node::printTreeInternal(std::vector<Node*> nodes, int level, int maxLevel) {
-  if (nodes.empty() || isAllElementsNull(nodes))
-    return;
-  
-  int floor = maxLevel - level;
-  int endgeLines = 1<<std::max(floor - 1, 0);
-  int firstSpaces = (1<<floor) - 1;
-  int betweenSpaces = (1<<(floor + 1)) - 1;
-  
-  std::cout << std::string(firstSpaces,' ');
+	if (nodes.empty() || isAllElementsNull(nodes))
+		return;
 
-  std::vector<Node*> newNodes;
-  for(std::vector<Node*>::iterator it = nodes.begin(); it != nodes.end(); ++it) {
-    if((*it) != NULL){
-      std::cout << (*it)->getData();
-      newNodes.push_back((*it)->getLeft());
-      newNodes.push_back((*it)->getRight());
-    }else{
-      newNodes.push_back(NULL);
-      newNodes.push_back(NULL);
-      std::cout << " ";
-    }
-    std::cout << std::string(betweenSpaces,' ');
-  }
-  
-  std::cout << std::endl;
+	int floor = maxLevel - level;
+	int endgeLines = 1<<std::max(floor - 1, 0);
+	int firstSpaces = (1<<floor) - 1;
+	int betweenSpaces = (1<<(floor + 1)) - 1;
 
-  for (int i = 1; i <= endgeLines; i++) {
-    for (unsigned int j = 0; j < nodes.size(); j++) {
-      if(firstSpaces - i > 0)
-	std::cout << std::string(firstSpaces - i,' ');
+	std::cout << std::string(firstSpaces,' ');
+	std::vector<Node*> newNodes;
+	
+	for(std::vector<Node*>::iterator it = nodes.begin(); it != nodes.end(); ++it) {
+		if((*it) != NULL){
+			std::cout << (*it)->getData();
+		newNodes.push_back((*it)->getLeft());
+		newNodes.push_back((*it)->getRight());
+		}else{
+			newNodes.push_back(NULL);
+		newNodes.push_back(NULL);
+		std::cout << " ";
+		}
+		std::cout << std::string(betweenSpaces,' ');
+	}
+	std::cout << std::endl;
+	
+	for (int i = 1; i <= endgeLines; i++) {
+		for (unsigned int j = 0; j < nodes.size(); j++) {
+			if(firstSpaces - i > 0)
+				std::cout << std::string(firstSpaces - i,' ');
 
-      if (nodes[j] == NULL) {
-	std::cout << std::string(endgeLines + endgeLines + i + 1,' ');
-	continue;
-      }
+			if (nodes[j] == NULL) {
+				std::cout << std::string(endgeLines + endgeLines + i + 1,' ');
+			continue;
+			}
+			if (nodes[j]->getLeft() != NULL)
+				std::cout << "/";
+			else	
+				std::cout << std::string(1,' ');
+			std::cout << std::string(i + i - 1,' ');
 
-      if (nodes[j]->getLeft() != NULL)
-	std::cout << "/";
-      else	
-	std::cout << std::string(1,' ');
-
-      std::cout << std::string(i + i - 1,' ');
-
-      if (nodes[j]->getRight() != NULL)
-	std::cout << "\\";
-      else
-	std::cout << std::string(1,' ');
-
-
-      std::cout << std::string(endgeLines + endgeLines - i,' ');
-    }
-
-    std::cout << std::endl;
-  }
-
-  printTreeInternal(newNodes, level + 1, maxLevel);
+			if (nodes[j]->getRight() != NULL)
+				std::cout << "\\";
+			else
+				std::cout << std::string(1,' ');
+			std::cout << std::string(endgeLines + endgeLines - i,' ');
+		}
+	std::cout << std::endl;
+	}
+	
+	printTreeInternal(newNodes, level + 1, maxLevel);
 }
 
 
@@ -118,7 +110,7 @@ bool Node::isAllElementsNull(std::vector<Node*> list) {
 Node* Node::differentiate(std::string var)
 {
 	Node* newNode=new Node();
-	
+
 	return differentiate(var,this,newNode);
 }
 
